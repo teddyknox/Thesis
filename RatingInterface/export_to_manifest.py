1c36e50d773c6e4c963e6b3067438cf2300a863d
Teddy Knox <teddyknox@gmail.com>
2015-04-14 12:53:55 -0400

    Save

--- RatingInterface/export_to_manifest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from models import Image
-import peewee
-
-with open('manifest.txt', 'w') as manifest:
-    # ratings = Rating.select().join(Image).order_by(Image.filename.desc())
-    images = Image.select()
-    for image in images:
-        manifest.write("/{}\t{:.2f}\n".format(image.filename,image.score))
