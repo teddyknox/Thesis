f561fcd21ce87582b96380d629413939bb9aaa42
Teddy Knox <teddyknox@gmail.com>
2015-04-14 11:18:40 -0400

    Export to lmdb

--- RatingInterface/export_to_manifest.py
+++ RatingInterface/export_to_manifest.py
@@ -6,4 +6,4 @@ with open('manifest.txt', 'w') as manifest:
     # ratings = Rating.select().join(Image).order_by(Image.filename.desc())
     images = Image.select()
     for image in images:
-        manifest.write("{}\t{:.2f}\n".format(image.filename,image.score))
+        manifest.write("/{}\t{:.2f}\n".format(image.filename,image.score))
