cd7b94e9040e09b6a24757cf99b7f0aefe8299b0
Teddy Knox <teddyknox@gmail.com>
2015-05-06 22:36:31 +0000

    Saving

--- src/classifiers/googlenet/resume_training.sh
+++ src/classifiers/googlenet/resume_training.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 /home/teddy/caffe/build/tools/caffe train \
-    --solver=./model/solver.prototxt \
-    --snapshot=./model/bvlc_googlenet_cae_iter_4000.solverstate \
+    --solver=./solver.prototxt \
+    --snapshot=./bvlc_googlenet_cae_iter_116000.solverstate \
     --gpu=0
