29b658f508349d158e6fd34abe6577c528eaa8a5
Teddy Knox <teddyknox@gmail.com>
2015-05-06 19:05:45 -0400

    Save

--- src/tasks/score_all_images.py
+++ src/tasks/score_all_images.py
@@ -12,13 +12,13 @@ db = SqliteDatabase(os.path.join(APP_DIRNAME, '/data/images.db'))
 BATCH_SIZE = 32
 
 to_score = []
-for image in Image.select():
-    if image.model_score == None or image.model_prediction == None:
-        exists = os.path.isfile(APP_DIRNAME + '/data/images/' + image.filename)
-        if exists:
-            to_score.append(image)
-        else:
-            image.delete()
+for image in Image.select().where(Image.model_score == None or Image.model_prediction == None):
+    exists = os.path.isfile(APP_DIRNAME + '/data/images/' + image.filename)
+    if exists:
+        to_score.append(image)
+    else:
+        image.delete()
+
 for i in range(0, len(to_score), BATCH_SIZE):
     batch = to_score[i:i+BATCH_SIZE]
     images = [caffe.io.load_image(os.path.join(IMAGES_DIR, filename))
@@ -34,8 +34,8 @@ correct = 0
 false_positive = 0
 false_negative = 0
 for image in Image.select().where(Image.num_ratings > 0):
-    if image.model_prediction and image.score > 0 or
-        not image.model_prediction and images.score == 0:
+    if image.model_prediction and image.score > 0 or \
+        not image.model_prediction and image.score == 0:
         correct += 1
     elif image.model_prediction and image.score == 0:
         false_positive += 1
