2f2fb4057d7cebd9ab42beb6745439ff9eb69c8f
Teddy Knox <teddyknox@gmail.com>
2015-05-09 13:18:22 -0400

    Save

--- src/experiments/googlenet/kfolds_googlenet.sh
+++ src/experiments/googlenet/kfolds_googlenet.sh
@@ -12,10 +12,11 @@ for FOLD in $(seq 1 $NUM_FOLDS); do
   FOLD_DIR=./folds/$FOLD
   mkdir -p $FOLD_DIR
   $SCRIPTS_DIR/write_manifests.py $FOLD $NUM_FOLDS $FOLD_DIR
+  mkdir $FOLD_DIR/train_lmdb $FOLD_DIR/test_lmdb
   $CAFFE_DIR/convert_imageset --shuffle ../images $FOLD_DIR/train.txt $FOLD_DIR/train_lmdb
   $CAFFE_DIR/convert_imageset --shuffle ../images $FOLD_DIR/val.txt $FOLD_DIR/val_lmdb
   cp solver.prototxt $FOLD_DIR/solver.prototxt
-  cp train_val.prototxt > $FOLD_DIR/train_val.prototxt
+  cp train_val.prototxt $FOLD_DIR/train_val.prototxt
   break
   $CAFFE_DIR/caffe train --solver=./solver.prototxt --gpu=0
 done
