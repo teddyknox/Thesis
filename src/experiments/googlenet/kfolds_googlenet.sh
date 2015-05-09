1e71b749655d574b06cce3bf4ca609e9d08d91a7
Teddy Knox <teddyknox@gmail.com>
2015-05-09 17:18:42 -0400

    Save

--- src/experiments/googlenet/kfolds_googlenet.sh
+++ src/experiments/googlenet/kfolds_googlenet.sh
@@ -13,15 +13,15 @@ NUM_FOLDS=10
 
 for FOLD in $(seq 1 $NUM_FOLDS); do
   FOLD_DIR=./folds/$FOLD
-  rm -rf $FOLD_DIR/train_lmdb $FOLD_DIR/val_lmdb $FOLD_DIR/logs $FOLD_DIR/snapshots
-  mkdir -p $FOLD_DIR $FOLD_DIR/snapshots $FOLD_DIR/logs
+  # rm -rf $FOLD_DIR/train_lmdb $FOLD_DIR/val_lmdb $FOLD_DIR/logs $FOLD_DIR/snapshots
+  # mkdir -p $FOLD_DIR $FOLD_DIR/snapshots $FOLD_DIR/logs
   pushd $FOLD_DIR
-  $SCRIPTS_DIR/write_manifests.py $FOLD $NUM_FOLDS .
-  $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR train.txt train_lmdb
-  $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR val.txt val_lmdb
-  $CAFFE_DIR/caffe train --solver=../../solver.prototxt --gpu=0 --log_dir=logs
+  # $SCRIPTS_DIR/write_manifests.py $FOLD $NUM_FOLDS .
+  # $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR train.txt train_lmdb
+  # $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR val.txt val_lmdb
+  # $CAFFE_DIR/caffe train --solver=../../solver.prototxt --gpu=0 --log_dir=logs
   WEIGHTS=$(ls snapshots/* | grep caffemodel | sort -r | head -n 1)
-  $CAFFE_DIR/caffe test --model=../../train_val.txt --weights=$WEIGHTS --gpu=0 --log_dir=log
+  $CAFFE_DIR/caffe test --model=../../train_val.prototxt --weights=$WEIGHTS --gpu=0 --log_dir=logs
   # LOGFILE=$(ls -t logs/* | grep .log.INFO | head -n 1)
   # cp $LOGFILE logs/train.log
   # $EXPERIMENT_DIR/plot_training_log.py 0 test_accuracy_vs_iters.png logs/train.log
@@ -29,6 +29,7 @@ for FOLD in $(seq 1 $NUM_FOLDS); do
   # $EXPERIMENT_DIR/plot_training_log.py 4 train_lr_vs_iters.png logs/train.log
   # $EXPERIMENT_DIR/plot_training_log.py 6 train_loss_vs_iters.png logs/train.log
   popd
+  break  
 done
 popd
 
