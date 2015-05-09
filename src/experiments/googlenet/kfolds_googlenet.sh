4980f5e4cbbca4fd2e1870ebe4a12817a7508678
Teddy Knox <teddyknox@gmail.com>
2015-05-09 16:06:14 -0400

    Save

--- src/experiments/googlenet/kfolds_googlenet.sh
+++ src/experiments/googlenet/kfolds_googlenet.sh
@@ -3,7 +3,8 @@
 # Quit on error code
 set -e
 
-pushd $(dirname $0)
+EXPERIMENT_DIR=$(dirname $0)
+pushd EXPERIMENT_DIR
 CAFFE_DIR=$HOME/caffe/build/tools
 SCRIPTS_DIR=$(readlink -f ../../scripts)
 IMAGES_DIR=$(readlink -f ../../images)/
@@ -12,21 +13,22 @@ NUM_FOLDS=10
 
 for FOLD in $(seq 1 $NUM_FOLDS); do
   FOLD_DIR=./folds/$FOLD
+  rm -rf train_lmdb val_lmdb $FOLD_DIR/logs $FOLD_DIR/snapshots
   mkdir -p $FOLD_DIR $FOLD_DIR/snapshots $FOLD_DIR/logs
   pushd $FOLD_DIR
-  rm -rf train_lmdb val_lmdb
   $SCRIPTS_DIR/write_manifests.py $FOLD $NUM_FOLDS .
   $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR train.txt train_lmdb
   $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR val.txt val_lmdb
   $CAFFE_DIR/caffe train --solver=../../solver.prototxt --gpu=0 --log_dir=logs
-  logfile=$(ls -t logs | grep .log.INFO | head -n 1)
-  mv logs/$logfile logs/train.log
-  plot_training_log.py 0 test_accuracy_vs_iters.png logs/train.log
-  plot_training_log.py 2 test_loss_vs_iters.png logs/train.log
+  LOGFILE=$(ls -t logs/* | grep .log.INFO | head -n 1)
+  cp $LOGFILE logs/train.log
+  $EXPERIMENT_DIR/plot_training_log.py 0 test_accuracy_vs_iters.png logs/train.log
+  $EXPERIMENT_DIR/plot_training_log.py 2 test_loss_vs_iters.png logs/train.log
   plot_training_log.py 4 train_lr_vs_iters.png logs/train.log
   plot_training_log.py 6 train_loss_vs_iters.png logs/train.log
+  WEIGHTS=$(ls snapshots/* | grep caffemodel | sort -r | head -n 1)
+  $CAFFE_DIR/caffe test --model=../../train_val.txt --weights=$WEIGHTS --gpu=0 --log_dir=log
   popd
-  # $CAFFE_DIR/caffe test --model=$FOLD_DIR/train_val.prototxt --weights=googlenet_iter_116000.caffemodel --gpu=0 --log_dir=$FOLD_DIR
 done
 popd
 
