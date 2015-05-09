608d1f070eb0119c829cbe112573fd287a8151ba
Teddy Knox <teddyknox@gmail.com>
2015-05-09 12:56:19 -0400

    Save

--- src/experiments/googlenet/kfolds_googlenet.sh
+++ src/experiments/googlenet/kfolds_googlenet.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# render a template configuration file
-# expand variables + preserve formatting
-render_template() {
-  eval "echo \"$(cat $1)\""
-}
-
 CAFFE_DIR=$HOME/caffe/build/tools
 SCRIPTS_DIR=../../scripts
 
@@ -17,9 +11,8 @@ for FOLD in $(seq 1 $NUM_FOLDS); do
   $SCRIPTS_DIR/write_manifests.py $FOLD $NUM_FOLDS $FOLD_DIR
   $CAFFE_DIR/convert_imageset --shuffle ../images $FOLD_DIR/train.txt $FOLD_DIR/train_lmdb
   $CAFFE_DIR/convert_imageset --shuffle ../images $FOLD_DIR/val.txt $FOLD_DIR/val_lmdb
-  # we may not need templating
-  render_template solver.prototxt.template > $FOLD_DIR/solver.prototxt
-  render_template train_val.prototxt.template > $FOLD_DIR/train_val.prototxt
+  cp solver.prototxt $FOLD_DIR/solver.prototxt
+  cp train_val.prototxt > $FOLD_DIR/train_val.prototxt
   break
   $CAFFE_DIR/caffe train --solver=./solver.prototxt --gpu=0
 done
