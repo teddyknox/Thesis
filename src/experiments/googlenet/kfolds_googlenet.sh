4c4c3063479c9531078f4719a64d0f4356aa2a71
Teddy Knox <teddyknox@gmail.com>
2015-05-09 15:34:43 -0400

    Save

--- src/experiments/googlenet/kfolds_googlenet.sh
+++ src/experiments/googlenet/kfolds_googlenet.sh
@@ -19,11 +19,12 @@ for FOLD in $(seq 1 $NUM_FOLDS); do
   $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR train.txt train_lmdb
   $CAFFE_DIR/convert_imageset --shuffle $IMAGES_DIR val.txt val_lmdb
   $CAFFE_DIR/caffe train --solver=../../solver.prototxt --gpu=0 --log_dir=logs
-  logfile=$(head -n 1 $(ls -t logs | grep .log.INFO))
-  plot_training_log.py 0 test_accuracy_vs_iters.png $(logs/*.log*)
-  plot_training_log.py 2 test_loss_vs_iters.png $(logs/*.log*)
-  plot_training_log.py 4 train_lr_vs_iters.png $(logs/*.log*)
-  plot_training_log.py 6 train_loss_vs_iters.png $(logs/*.log*)
+  logfile=$(ls -t logs | grep .log.INFO | head -n 1)
+  mv logs/$logfile logs/train.log
+  plot_training_log.py 0 test_accuracy_vs_iters.png logs/train.log
+  plot_training_log.py 2 test_loss_vs_iters.png logs/train.log
+  plot_training_log.py 4 train_lr_vs_iters.png logs/train.log
+  plot_training_log.py 6 train_loss_vs_iters.png logs/train.log
   popd
   # $CAFFE_DIR/caffe test --model=$FOLD_DIR/train_val.prototxt --weights=googlenet_iter_116000.caffemodel --gpu=0 --log_dir=$FOLD_DIR
 done
